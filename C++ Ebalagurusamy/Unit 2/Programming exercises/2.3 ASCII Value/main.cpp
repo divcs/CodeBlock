b9f09e6d749245d756f9c53ccd72c61507b671ae
Divya <singhdivya10aug2002@gmail.com>
2024-11-09 20:33:01 +0530

    Display string n times

--- C++ Ebalagurusamy/Unit 2/Programming exercises/2.3 ASCII Value/main.cpp
+++ C++ Ebalagurusamy/Unit 2/Programming exercises/2.3 ASCII Value/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    char ch;
+
     return 0;
 }
---
+++ C++ Ebalagurusamy/Unit 2/Programming exercises/Some Extras/1 Display n times some string/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    int N;
+
+    cin>>s>>N;
+
+    for(int i=0; i<N; i++){
+        cout<<s<<endl;
+    }
+    return 0;
+}
