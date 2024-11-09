77cf9f1c8eee41953553ec6a34dff9c9f0a11c14
Divya <singhdivya10aug2002@gmail.com>
2024-11-09 20:49:13 +0530

    Ascii value

--- C++ Ebalagurusamy/Unit 2/Programming exercises/2.3 ASCII Value/main.cpp
+++ C++ Ebalagurusamy/Unit 2/Programming exercises/2.3 ASCII Value/main.cpp
@@ -5,6 +5,11 @@ using namespace std;
 int main()
 {
     char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    int value = ch;
+    cout << "ASCII value: " << value << endl;
 
     return 0;
 }
