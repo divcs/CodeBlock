8cce8dd5fbb44935629f334b036cf62051faa3a1
Divya <singhdivya10aug2002@gmail.com>
2024-12-03 21:59:39 +0530

    updates

---
+++ C++ Ebalagurusamy/Unit 2/Programming exercises/L38/Q2/File_01_ReadFromFile/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    ifstream inputFile("names.txt"); // Open the file for reading
+    string name;
+
+    if (inputFile.is_open()) {
+        cout << "Names in the file:\n";
+        while (getline(inputFile, name)) {  // Read line by line
+            cout << name << endl;           // Display each name
+        }
+        inputFile.close();  // Close the file
+    } else {
+        cout << "Unable to open the file!" << endl;
+    }
+    return 0;
+}
